d000a42b181793ed3e16a303507fd83a783132d2
Gid Baerridge <gid.berridge@gmail.com>
2021-07-31 15:09:56 +0100

    instruments controller first push

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,2 +1,43 @@
 class InstrumentsController < ApplicationController
+
+  def index
+    @instrument = Instrument.find(params[:instrument_type])
+    # @instrument = Instrument.all
+  end
+
+  def show
+    @instrument = Instrument.find(params[:id])
+  end
+
+  def new
+    @instrument = Instrument.new
+  end
+
+  def create
+    @instrument = Instrument.new(instrument_params)
+    if @instrument.save
+      redirect_to instrument_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @instrument = Instrument.find(params[:id])
+    @instrument.update(instrument_params)
+    redirect_to instruments_path
+  end
+
+  def destroy
+    @instrument = Instrument.find(params[:id])
+    @instrument.destroy
+    redirect_to instruments_path
+  end
+
+  private
+
+  def list_params
+    params.require(:instrument).permit(:instrument_type)
+  end
+
 end
