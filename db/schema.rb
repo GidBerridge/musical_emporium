463fa9edcd98e984c58ad5564e1ffe87a5f3454f
Gid Baerridge <gid.berridge@gmail.com>
2021-08-01 20:45:55 +0100

    74a7bee1a105506ea5880a0c0532f5d0ed9da07a

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-
 ActiveRecord::Schema.define(version: 2021_07_31_153804) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +33,6 @@ ActiveRecord::Schema.define(version: 2021_07_31_153804) do
     t.float "daily_rate"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-
     t.string "make"
     t.string "model_name"
     t.string "image_url"
@@ -52,7 +47,6 @@ ActiveRecord::Schema.define(version: 2021_07_31_153804) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["instrument_id"], name: "index_reviews_on_instrument_id"
-
   end
 
   create_table "users", force: :cascade do |t|
