f0d408118fad0c441e70b37c25e06fafcbdbc394
Clare Amos <clare.amos@gmail.com>
2021-08-03 21:36:26 +0100

    changed model name to instrument name and re-seeded

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_31_153804) do
+ActiveRecord::Schema.define(version: 2021_08_03_193625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,10 +33,10 @@ ActiveRecord::Schema.define(version: 2021_07_31_153804) do
     t.float "daily_rate"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "make"
-    t.string "model_name"
-    t.string "image_url"
     t.bigint "user_id"
+    t.string "make"
+    t.string "instrument_name"
+    t.string "image_url"
     t.index ["user_id"], name: "index_instruments_on_user_id"
   end
 
