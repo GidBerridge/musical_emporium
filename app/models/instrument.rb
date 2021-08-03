70925d2fcacdc3402a00f7c96d0a0c1c8a797281
Gid Baerridge <gid.berridge@gmail.com>
2021-08-03 12:32:11 +0100

    validations redone

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,3 +1,6 @@
 class Instrument < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+
+  validates :instrument_type, :delivery_option, :daily_rate, :make, :model_name, :image_url, presence: true
 end
