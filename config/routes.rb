2816806a416f188800b02ef7bdb1a390f3752db7
liz tobin <lizmtobin@gmail.com>
2021-08-03 20:06:00 +0100

    bookings controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+  get 'bookings/new'
+  get 'bookings/create'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :instruments do
