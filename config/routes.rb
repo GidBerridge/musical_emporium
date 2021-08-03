d3c056188d738161e6edca440561ce5b5f745c68
liz tobin <lizmtobin@gmail.com>
2021-08-03 21:31:44 +0100

    added form for booking

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
-  get 'bookings/new'
-  get 'bookings/create'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :instruments do
     resources :bookings, only: [ :new, :create ]
   end
-  resources :bookings, only: [ :index, :show, :edit, :update ]
 end
