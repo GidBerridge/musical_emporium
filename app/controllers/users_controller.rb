f4157a50a5456da4999f397c91136e1a1350d6d8
Clare Amos <clare.amos@gmail.com>
2021-08-09 08:49:15 +0100

    changed user index and show controller actions and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
 
-  def show
+  def index
+    @instruments = current_user.instruments
     @bookings = current_user.bookings
+  end
+
+  def show
+    @instrument = Instrument.find(params[:instrument_id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   end
   devise_for :users
 
-  get "profile", action: :show, controller: "users"
+  get "profile", action: :index, controller: "users"
 end
