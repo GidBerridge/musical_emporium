3f6a1e3e4e75ef1b6a8a04c3c473fde44c529d86
Clare Amos <clare.amos@gmail.com>
2021-08-10 08:53:26 +0100

    edit and delete set up for instruments, and shared show page to allow different links

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :instruments do
-    resources :bookings, only: [ :new, :create, :index]
+    resources :bookings, only: [ :new, :create, :index ]
   end
   devise_for :users
 
