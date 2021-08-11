f398a18e5e937709ac5f057405ba6747d89a49e2
liz tobin <lizmtobin@gmail.com>
2021-08-11 21:25:04 +0100

    migrated geocode long lat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_07_133115) do
+ActiveRecord::Schema.define(version: 2021_08_10_195825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 2021_08_07_133115) do
     t.string "instrument_name"
     t.string "image_url"
     t.bigint "user_id"
+    t.text "description"
+    t.string "category"
     t.index ["user_id"], name: "index_instruments_on_user_id"
   end
 
@@ -76,9 +78,7 @@ ActiveRecord::Schema.define(version: 2021_08_07_133115) do
     t.bigint "instrument_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
     t.index ["instrument_id"], name: "index_reviews_on_instrument_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -102,5 +102,5 @@ ActiveRecord::Schema.define(version: 2021_08_07_133115) do
   add_foreign_key "bookings", "instruments"
   add_foreign_key "bookings", "users"
   add_foreign_key "instruments", "users"
-  add_foreign_key "reviews", "users"
+  add_foreign_key "reviews", "instruments"
 end
