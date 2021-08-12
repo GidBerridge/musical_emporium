651e6a28097e13eadff8140de9cff99dd8178916
liz tobin <lizmtobin@gmail.com>
2021-08-12 10:18:19 +0100

    made changes to JS to make the login and map work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import { initFlatpickr } from "../plugins/flatpickr";
 
-initFlatpickr();
 
 Rails.start()
 Turbolinks.start()
@@ -27,15 +25,14 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
-
+import { initFlatpickr } from "../plugins/flatpickr";
 
 import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
+  initFlatpickr();
+
 })
+
 
